01489a2df73a808c594eae1dea5ce2763bc1f16d
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2012-04-14 05:59:32 +0000

    version 0.3.5 from TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "fh_debug".
 #
-# Auto generated 28-03-2012 19:11
+# Auto generated 14-04-2012 07:57
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'PHP debugger',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.3.4',
+	'version' => '0.3.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"be32";s:21:"ext_conf_template.txt";s:4:"d235";s:12:"ext_icon.gif";s:4:"fd3c";s:17:"ext_localconf.php";s:4:"7b08";s:10:"manual.sxw";s:4:"68ac";s:14:"doc/manual.sxw";s:4:"abd7";s:24:"lib/class.tx_fhdebug.php";s:4:"c31d";s:15:"res/fhdebug.css";s:4:"1c94";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"f379";s:21:"ext_conf_template.txt";s:4:"f481";s:12:"ext_icon.gif";s:4:"fd3c";s:17:"ext_localconf.php";s:4:"2189";s:10:"manual.sxw";s:4:"68ac";s:14:"doc/manual.sxw";s:4:"abd7";s:24:"lib/class.tx_fhdebug.php";s:4:"f2e9";s:15:"res/fhdebug.css";s:4:"1c94";}',
 	'suggests' => array(
 	),
 );
