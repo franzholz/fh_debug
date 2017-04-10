3a10c4f7c067ea4c26d7061a1ca90cf8dc2cd36d
Franz Holzinger <holzinger@ttproducts.de>
2017-04-10 20:22:34 +0200

    remove the _md5_values_when_last_written from ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,8 +36,5 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"aea7";s:16:"ext_autoload.php";s:4:"eeac";s:21:"ext_conf_template.txt";s:4:"7833";s:12:"ext_icon.gif";s:4:"fd3c";s:17:"ext_localconf.php";s:4:"a9dd";s:14:"ext_tables.sql";s:4:"d41d";s:14:"doc/manual.sxw";s:4:"abd7";s:35:"hooks/class.tx_fhdebug_hooks_em.php";s:4:"e20c";s:24:"lib/class.tx_fhdebug.php";s:4:"f79f";s:15:"res/fhdebug.css";s:4:"1c94";}',
-	'suggests' => array(
-	),
 );
 
