ffcfe0219ce9536894a314fb216ccb25b28bd664
Franz Holzinger <holzinger@ttproducts.de>
2020-01-10 13:59:01 +0100

    next version 0.8.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Debug generator and Devlog',
     'description' => 'This generates a PHP debug output file. And this is a sys_log and devLog data logger',
     'category' => 'misc',
-    'version' => '0.8.1',
+    'version' => '0.8.2',
     'state' => 'stable',
     'uploadfolder' => '',
     'createDirs' => '',
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'jambage.com',
     'constraints' => array(
         'depends' => array(
-            'php' => '7.0.0-7.99.99',
+            'php' => '7.0.0-7.3.99',
             'typo3' => '8.7.0-9.5.99',
         ),
         'conflicts' => array(
