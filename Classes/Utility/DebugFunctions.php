61d6b93e2bc485a6b40dcc0940341c5273fc6917
Franz Holzinger <holzinger@ttproducts.de>
2021-02-09 22:34:05 +0100

    * remove feature: The Content-Security-Policy in this file cannot supersede the .htacess files. So it makes no sense to add it here.

--- Classes/Utility/DebugFunctions.php
+++ Classes/Utility/DebugFunctions.php
@@ -545,7 +545,6 @@ class DebugFunctions {
         ) {
             $title .= ' id=' . $GLOBALS['TSFE']->id;
         }
-        $host = static::getHost() . $_SERVER['CWD'];
 
         $out = '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -553,8 +552,6 @@ class DebugFunctions {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>' . $title . '</title>
-<meta http-equiv="Content-Security-Policy" 
-    content="default-src *; style-src \'self\' ' . $host . ';"/>
 <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
 <link href="' . $cssFilename . '" rel="stylesheet" media="screen" type="text/css"/>
 </head>
