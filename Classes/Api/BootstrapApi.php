f17bb52ec6fedb144dd59a165d39ed8ef978e164
Franz Holzinger <franz.holzinger@festwein.de>
2023-07-05 13:07:14 +0200

    add class BootstrapApi

---
+++ Classes/Api/BootstrapApi.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace JambageCom\FhDebug\Api;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Http\ApplicationType;
+use TYPO3\CMS\Core\Log\LogLevel;
+use TYPO3\CMS\Core\Log\Writer\FileWriter;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+
+use JambageCom\FhDebug\Configuration\Variant;
+use JambageCom\FhDebug\Hooks\DBALException;
+use JambageCom\FhDebug\Hooks\ProductionExceptionHandler;
+use JambageCom\FhDebug\Hooks\TsparserHooks;
+use JambageCom\FhDebug\Log\Writer\LogWriter;
+use JambageCom\FhDebug\Utility\DebugFunctions;
+
+/**
+ * Components for the Debug
+ */
+class BootstrapApi
+{
+    /**
+     * initialize the global debug object
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function init (ServerRequestInterface $request)
+    {
+        if (!defined('FH_DEBUG_EXT')) {
+            define('FH_DEBUG_EXT', 'fh_debug');
+        }
+
+        $extensionConfiguration = GeneralUtility::makeInstance(
+            ExtensionConfiguration::class
+        )->get(FH_DEBUG_EXT);
+
+        if (
+            isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT]) &&
+            is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT])
+        ) {
+            $tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT];
+        }
+
+        if (is_array($extensionConfiguration)) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT] = $extensionConfiguration;
+            if (isset($tmpArray) && is_array($tmpArray)) {
+                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT] = array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT], $tmpArray);
+            }
+        }
+
+        if (
+            isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT]) &&
+            is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT])
+        ) {
+            $class = DebugFunctions::class;
+            if (
+                !isset($GLOBALS['error']) ||
+                !is_object($GLOBALS['error']) ||
+                !($GLOBALS['error'] instanceof $class) ||
+                !$GLOBALS['error']->hasBeenInitialized()
+            ) {
+                $myDebugObject = null;
+                $newExtConf = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT];
+                $currentTypo3Mode = (ApplicationType::fromRequest($request)->isFrontend() ? 'FE' : 'BE');
+
+                if (
+                    !isset($GLOBALS['error']) ||
+                    !($GLOBALS['error'] instanceof $class)
+                ) {
+                    $config = $newExtConf;
+                    $config['default'] = 1;
+                    $myConfiguratinVariant =
+                        GeneralUtility::makeInstance(
+                            Variant::class,
+                            $config
+                        );
+                    // New operator used on purpose: This class is required early during
+                    // bootstrap before makeInstance() is properly set up
+                    $myDebugObject =
+                        GeneralUtility::makeInstance(
+                            DebugFunctions::class,
+                            $newExtConf,
+                            $currentTypo3Mode
+                        );
+                    // The contructor contains important static initializations which are needed immediately.
+                }
+                $ipIsAllowed = false;
+                $ipAdress = DebugFunctions::initIpAddress($ipIsAllowed);
+                $modeIsAllowed = DebugFunctions::verifyTypo3Mode($currentTypo3Mode);
+                $initResult = false;
+
+                if (
+                    $modeIsAllowed &&
+                    (
+                        $ipIsAllowed ||
+                        GeneralUtility::cmpIP($ipAdress, '127.0.0.1') ||
+                        $typo3Mode == 'FE' && $newExtConf['FEUSERNAMES']
+                    )
+                ) {
+                    $GLOBALS['TYPO3_CONF_VARS']['LOG']['JambageCom']['FhDebug']['writerConfiguration'] = [
+                        LogLevel::DEBUG => [
+                                FileWriter::class => [
+                                'logFileInfix' => 'debug'
+                            ]
+                        ]
+                    ];
+
+                    if (
+                        isset($GLOBALS['error']) &&
+                        $GLOBALS['error'] instanceof $class
+                    ) {
+                        $GLOBALS['error']->init($ipAdress);
+                    } else {
+                        $initResult = DebugFunctions::init($ipAdress);
+                    }
+
+                    if ($newExtConf['OOPS_AN_ERROR_OCCURRED']) {
+                        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][ProductionExceptionHandler::class] = [
+                            'className' => ProductionExceptionHandler::class
+                        ];
+                    }
+
+                    if ($newExtConf['DBAL']) {
+                        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Doctrine\\DBAL\\DBALException'] = [
+                            'className' => DBALException::class
+                        ];
+                    }
+
+                    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tsparser']['configurationParser'][FH_DEBUG_EXT] = TsparserHooks::class;
+                }
+
+                if (
+                    is_object($myDebugObject) &&
+                    (
+                        !isset($GLOBALS['error']) ||
+                        !($GLOBALS['error'] instanceof $class)
+                    )
+                ) {
+                // the error object must always be set in order to show the debug output or to disable it
+                    $GLOBALS['error'] = $myDebugObject;
+                }
+
+                $logLevel = -1;
+                if (MathUtility::canBeInterpretedAsInteger($newExtConf['LOGLEVEL'])) {
+                    $logLevel = intval($newExtConf['LOGLEVEL']);
+                }
+
+                if (
+                    $logLevel >= LogLevel::EMERGENCY &&
+                    $logLevel <= LogLevel::DEBUG
+                ) {
+                    $originalWriters = '';
+                    // configuration for DEBUG severity, including all
+                    // levels with higher severity (DEBUG, DEBUG, ERROR, CRITICAL, EMERGENCY)
+                    if (!isset($GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][$logLevel])) {
+                        $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][$logLevel] = [];
+                    }
+
+                    $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration']
+                        [$logLevel][LogWriter::class] = [];
+                }
+            }
+        }    
+    }
+}
+
--- Classes/Middleware/Bootstrap.php
+++ Classes/Middleware/Bootstrap.php
@@ -23,7 +23,8 @@ use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
- 
+use JambageCom\FhDebug\Api\BootstrapApi;
+
 /**
  * Initializes the global ERROR object before processing a request for the TYPO3 Frontend.
  *
@@ -39,143 +40,10 @@ class Bootstrap implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (!defined('FH_DEBUG_EXT')) {
-            define('FH_DEBUG_EXT', 'fh_debug');
-        }
+ error_log('JambageCom\FhDebug\Middleware\Bootstrap::process Pos 1: ' .  static::print_r($request, true) . PHP_EOL, 3, '/var/www/html/fileadmin/phpDebugErrorLog.txt');
 
-        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-            \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-        )->get(FH_DEBUG_EXT);
-
-        if (
-            isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT]) &&
-            is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT])
-        ) {
-            $tmpArray = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT];
-        } else {
-            unset($tmpArray);
-        }
-
-        if (is_array($extensionConfiguration)) {
-            $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT] = $extensionConfiguration;
-            if (isset($tmpArray) && is_array($tmpArray)) {
-                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT] = array_merge($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT], $tmpArray);
-            }
-        }
-
-        if (
-            isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT]) &&
-            is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT])
-        ) {
-            $class = \JambageCom\FhDebug\Utility\DebugFunctions::class;
-            if (
-                !isset($GLOBALS['error']) ||
-                !is_object($GLOBALS['error']) ||
-                !($GLOBALS['error'] instanceof $class) ||
-                !$GLOBALS['error']->hasBeenInitialized()
-            ) {
-                $myDebugObject = null;
-                $newExtConf = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][FH_DEBUG_EXT];
-                $currentTypo3Mode = (ApplicationType::fromRequest($request)->isFrontend() ? 'FE' : 'BE');
-
-                if (
-                    !isset($GLOBALS['error']) ||
-                    !($GLOBALS['error'] instanceof $class)
-                ) {
-                    $config = $newExtConf;
-                    $config['default'] = 1;
-                    $myConfiguratinVariant =
-                        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-                            \JambageCom\FhDebug\Configuration\Variant::class,
-                            $config
-                        );
-                    // New operator used on purpose: This class is required early during
-                    // bootstrap before makeInstance() is properly set up
-                    $myDebugObject =
-                        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-                            \JambageCom\FhDebug\Utility\DebugFunctions::class,
-                            $newExtConf,
-                            $currentTypo3Mode
-                        );
-                    // The contructor contains important static initializations which are needed immediately.
-                }
-                $ipIsAllowed = false;
-                $ipAdress = \JambageCom\FhDebug\Utility\DebugFunctions::initIpAddress($ipIsAllowed);
-                $modeIsAllowed = \JambageCom\FhDebug\Utility\DebugFunctions::verifyTypo3Mode($currentTypo3Mode);
-                $initResult = false;
-
-                if (
-                    $modeIsAllowed &&
-                    (
-                        $ipIsAllowed ||
-                        \TYPO3\CMS\Core\Utility\GeneralUtility::cmpIP($ipAdress, '127.0.0.1') ||
-                        $typo3Mode == 'FE' && $newExtConf['FEUSERNAMES']
-                    )
-                ) {
-                    $GLOBALS['TYPO3_CONF_VARS']['LOG']['JambageCom']['FhDebug']['writerConfiguration'] = [
-                        \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-                                \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
-                                'logFileInfix' => 'debug'
-                            ]
-                        ]
-                    ];
-
-                    if (
-                        isset($GLOBALS['error']) &&
-                        $GLOBALS['error'] instanceof $class
-                    ) {
-                        $GLOBALS['error']->init($ipAdress);
-                    } else {
-                        $initResult = \JambageCom\FhDebug\Utility\DebugFunctions::init($ipAdress);
-                    }
-
-                    if ($newExtConf['OOPS_AN_ERROR_OCCURRED']) {
-                        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Frontend\ContentObject\Exception\ProductionExceptionHandler::class] = [
-                            'className' => \JambageCom\FhDebug\Hooks\ProductionExceptionHandler::class
-                        ];
-                    }
-
-                    if ($newExtConf['DBAL']) {
-                        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Doctrine\\DBAL\\DBALException'] = [
-                            'className' => \JambageCom\FhDebug\Hooks\DBALException::class
-                        ];
-                    }
-
-                    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tsparser']['configurationParser'][FH_DEBUG_EXT] = \JambageCom\FhDebug\Hooks\TsparserHooks::class;
-                }
-
-                if (
-                    is_object($myDebugObject) &&
-                    (
-                        !isset($GLOBALS['error']) ||
-                        !($GLOBALS['error'] instanceof $class)
-                    )
-                ) {
-                // the error object must always be set in order to show the debug output or to disable it
-                    $GLOBALS['error'] = $myDebugObject;
-                }
-
-                $logLevel = -1;
-                if (\TYPO3\CMS\Core\Utility\MathUtility::canBeInterpretedAsInteger($newExtConf['LOGLEVEL'])) {
-                    $logLevel = intval($newExtConf['LOGLEVEL']);
-                }
-
-                if (
-                    $logLevel >= \TYPO3\CMS\Core\Log\LogLevel::EMERGENCY &&
-                    $logLevel <= \TYPO3\CMS\Core\Log\LogLevel::DEBUG
-                ) {
-                    $originalWriters = '';
-                    // configuration for DEBUG severity, including all
-                    // levels with higher severity (DEBUG, DEBUG, ERROR, CRITICAL, EMERGENCY)
-                    if (!isset($GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][$logLevel])) {
-                        $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][$logLevel] = [];
-                    }
-
-                    $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration']
-                        [$logLevel][\JambageCom\FhDebug\Log\Writer\LogWriter::class] = [];
-                }
-            }
-        }
+        $api = GeneralUtility::makeInstance(BootstrapApi::class);
+        $api->init($request);
 
         return $handler->handle($request);
     }
