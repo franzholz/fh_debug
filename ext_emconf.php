d172fb7f33e08c6f43ee28b06f40fce476586deb
Franz Holzinger <franz.holzinger@festwein.de>
2023-07-24 10:39:29 +0200

    still works under TYPO3 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Debug generator and Log Writer',
     'description' => 'This generates a PHP debug output file. FileWriter into debug',
     'category' => 'misc',
-    'version' => '0.15.2',
+    'version' => '0.15.3',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Franz Holzinger',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '10.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
